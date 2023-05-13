3b736ed2bfd767f7834f452822a14bfcf98c7e91
DeepMakhasana <deepmakhasana1013@gmail.com>
2023-05-13 18:50:29 +0530

    Add Home route

--- backend/app.js
+++ backend/app.js
@@ -11,18 +11,21 @@ const postRouter = require("./router/post");
 const app = express();
 
 // dotenv
-dotenv.config({path: 'backend/config/.env'});
+dotenv.config({ path: "backend/config/.env" });
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
 // router
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome on Social media backend part. - <a href='https://github.com/DeepMakhasana/socialmedia_webapp'>Go to API Documentation</a>"
+  );
+});
 app.use("/api/v1/account", accountRouter);
 app.use("/api/v1/post", postRouter);
-
-
 
 module.exports = app;
