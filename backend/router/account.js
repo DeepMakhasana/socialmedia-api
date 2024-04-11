2ab08aa1c4a097572418c49cef78b8724567981d
DeepMakhasana <deepmakhasana1013@gmail.com>
2024-04-11 23:16:28 +0530

    fix: image upload path

--- backend/router/account.js
+++ backend/router/account.js
@@ -16,9 +16,7 @@ const {
 
 // multer setup
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "backend/upload/postImage");
-  },
+  destination: "./public/uploads/profileImages",
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "-" + file.originalname);
--- backend/router/post.js
+++ backend/router/post.js
@@ -14,9 +14,7 @@ const {
 
 // multer setup
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "../socialmedia/backend/upload/postImage");
-  },
+  destination: "./public/uploads/postImages",
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "-" + file.originalname);
